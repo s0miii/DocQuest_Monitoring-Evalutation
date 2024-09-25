6d29935fdb5e89966e47075a73aa68494384fc96
wynoahlouis <tabasan.wynoah@gmail.com>
2024-09-25 14:24:29 +0800

    Minor Changes in Colors

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,15 +22,15 @@ const NavBar = () => {
             {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
             
         </div>
-        <div className={!nav ? 'fixed left-0 top-0 w-[30%] h-full border-r border-r-yellow-400 bg-blue-700 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <div className={!nav ? 'fixed left-0 top-0 w-[30%] h-full border-r border-r-yellow-400 bg-custom-blue ease-in-out duration-500' : 'fixed left-[-100%]'}>
         <h1 className='w-full text-4xl font-bold text-yellow-500 m-4'>DocQuest</h1>
 
             <ul className='p-4'>
-                <li className='p-4'>Dashboard</li>
-                <li className='p-4'>User Management</li>
-                <li className='p-4'>Projects</li>
-                <li className='p-4'>Documents</li>
-                <li className='p-4'>Logout</li>
+                <li className='p-4 text-white'>Dashboard</li>
+                <li className='p-4 text-white'>User Management</li>
+                <li className='p-4 text-white'>Projects</li>
+                <li className='p-4 text-white'>Documents</li>
+                <li className='p-4 text-white'>Logout</li>
             </ul>
         </div>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#060E57',
+      },
+    },
   },
   plugins: [],
 }
