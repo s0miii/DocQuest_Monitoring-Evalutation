2fcd65df73f5cba5af1092cc2d1ed88bf215ab17
Clarice Domingo <domingo.clarice7@gmail.com>
2024-11-18 14:42:43 +0800

    Deleted Backend Folder locally

--- Backend/monitoring_evaluation/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class MonitoringEvaluationConfig(AppConfig):
-    name = 'monitoring_evaluation'
-
-    def ready(self):
-        import monitoring_evaluation.signals
