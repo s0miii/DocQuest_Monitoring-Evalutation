50d8e4089be3c284c727c5cf0ff36e2bee451fd6
Clarice Domingo <domingo.clarice7@gmail.com>
2024-11-28 11:26:33 +0800

    ...

--- Frontend/src/pages/estaff/EStaffProjects.js
+++ Frontend/src/pages/estaff/EStaffProjects.js
@@ -23,7 +23,7 @@ const EStaffProjects = () => {
                     <h1 className="text-2xl font-semibold mb-5">Projects Overview</h1>
 
                     {/* Approved, Pending, Rejected UI */}
-                    <div className="grid grid-cols-3 gap-4 mb-10">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-10">
                         {/* Not Yet Started */}
                         <div className="bg-orange-500 text-white rounded-lg p-6 flex flex-col items-center justify-center">
                             <h2 className="text-lg font-semibold">Not Yet Started</h2>
