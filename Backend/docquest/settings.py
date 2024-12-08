c8d27a2ac677b56611c19792f94bea80d41238c1
Keith Taps <keithtapongot0705>
2024-12-08 13:17:27 +0800

    Working feature of viewing file submitted when clicked

--- Backend/docquest/settings.py
+++ Backend/docquest/settings.py
@@ -134,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Media Files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
