2998ff4d5a5147ce2caf934fca005087d38f9c38
Keith Taps <keithtapongot0705>
2024-12-16 02:10:25 +0800

    Render variable fix

--- Backend/docquest/settings.py
+++ Backend/docquest/settings.py
@@ -162,10 +162,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this
-
-# Ensure staticfiles storage for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
