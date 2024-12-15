b6b668766c5923666bc2b60ebc36e2a2c7b46d7d
Keith Taps <keithtapongot0705>
2024-12-16 01:39:00 +0800

    wsgi fix for render

--- Backend/docquest/wsgi.py
+++ Backend/docquest/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docquest.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
