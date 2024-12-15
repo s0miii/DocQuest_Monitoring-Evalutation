872b86cd45d032ed4f7dfad48db118a6ce6df56b
Keith Taps <keithtapongot0705>
2024-12-16 02:10:25 +0800

    Render variable fix

--- Backend/docquest/wsgi.py
+++ Backend/docquest/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+from pathlib import Path  # Import Path for BASE_DIR
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings
+from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docquest.settings')
+# Define BASE_DIR
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application = WhiteNoise(application, root=os.path.join(str(BASE_DIR), 'staticfiles'))
+application.add_files(os.path.join(str(BASE_DIR), 'static'), prefix='static/')
