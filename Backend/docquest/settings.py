a81a147c2e44ee760176467bc56e998d12e1edf4
Keith Taps <keithtapongot0705>
2024-12-16 01:11:19 +0800

    More fixes for render

--- Backend/docquest/settings.py
+++ Backend/docquest/settings.py
@@ -163,7 +163,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Use ManifestStaticFilesStorage for production
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
