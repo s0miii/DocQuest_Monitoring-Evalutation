b70392cc48103d8732783122398f4a6bc1b456ae
Keith Taps <keithtapongot0705>
2024-12-16 02:23:20 +0800

    render more fixes

--- Backend/docquest/settings.py
+++ Backend/docquest/settings.py
@@ -109,7 +109,9 @@ DATABASES = {
     # }
 
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # Forces SSL connection
     )
 }
 
