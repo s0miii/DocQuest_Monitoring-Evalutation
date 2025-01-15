265ea9d275326e983aa6e7725f83064e9507fd72
Clarice Domingo <domingo.clarice7@gmail.com>
2025-01-16 00:03:18 +0800

    ....

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -95,7 +95,7 @@ function App() {
                   <Route path="/projlead/proj" element={<ProjLeadProjects />} />
                   <Route path="/projlead/proj/req/:projectID" element={<ProjLeadProjReq />} />
                   <Route path="/projlead/assign-checklist/:projectID" element={<ProjLeadAssignProponents />} />
-                  <Route path="/projlead/project/:projectID/accomplishment-report/:id" element={<ProjLeadAccReport />} />
+                  <Route path="/projlead/project/:projectID/accomplishment-report" element={<ProjLeadAccReport />} />
                   <Route path="/projlead/project/:projectID/daily-attendance" element={<ProjLeadDailyAttRec />} />
                   <Route path="/projlead/project/:projectID/summary-of-evaluation" element={<ProjLeadEvalSum />} />
                   <Route path="/projlead/project/:projectID/trainer-cv-dtr" element={<ProjLeadCvDtr />} />
